0be7297d78a6bea50cbf867ba7790e7136823a10
Islombek Orifov <islombekorifov0199@gmail.com>
2025-06-02 09:07:00 +0500

    structure models addded

--- account/models.py
+++ account/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    
+
     phone = models.CharField(max_length=15, blank=True, null=True)
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
